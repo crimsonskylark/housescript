9d7194db2c8b8e00fdd9eb4b860bea9881443470
crimsonskylark <47747084+crimsonskylark@users.noreply.github.com>
2020-07-14 06:31:30 -0300

    Base config management class.

---
+++ HouseScriptClient/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseScript.Client
+{
+    /*
+     * <summary>
+     * This class deals with all the configuration settings of the script.
+     * </summary>
+     */
+    class Config
+    {
+        public Config()
+        { }
+
+    }
+}
