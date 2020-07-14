278fc4afc8444d80ff76bdc19cc3dc36b7ccd37d
crimsonskylark <47747084+crimsonskylark@users.noreply.github.com>
2020-07-14 06:09:47 -0300

    Basic database interface working.

--- HouseScriptServer/Main.cs
+++ HouseScriptServer/Main.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using MySqlConnector;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace HouseScriptServer
+namespace HouseScript.Server
 {
-    public class HouseScriptServerMain
+    public class HouseScriptServer : BaseScript
     {
+
+        MySqlConnection dbInterface = new MySqlConnection(GetConvar("mysql_connection_string", ""));
+
+
+        public HouseScriptServer()
+        {}
+
+        [EventHandler("playerJoining")]
+        private void GetPlayerObjects([FromSource] Player player)
+        {
+            var playerRockstarIdentifier = player.Identifiers
+                                            .ToList()
+                                            .Where(e => e.StartsWith("license:"))
+                                            .First()
+                                            .Substring(8);
+            Debug.WriteLine($"Player name: {player.Name}\nIdentifier: {playerRockstarIdentifier}");
+        }
+
+        [EventHandler("onServerResourceStart")]
+        private void InitDb(string resName)
+        {
+            if (GetCurrentResourceName() != resName) { return; }
+            dbInterface.Open();
+
+            using (var command = dbInterface.CreateCommand())
+            {
+                command.CommandText = "SELECT * FROM items";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Debug.WriteLine(reader.GetString(0));
+                    }
+                }
+            }
+        }
+
+        [EventHandler("onServerResourceStart")]
+        private void LicenseCheck(string resName)
+        {
+            if (GetCurrentResourceName() != resName) { return; }
+            string licenseKey = GetConvar("vfms_LicenseKey", "");
+            if (licenseKey.Length < 1)
+            {
+                Debug.WriteLine("\n^1[!!]^9 ^0Your VFMS license key is^9 ^1invalid^9\n");
+            } else
+            {
+                Debug.WriteLine("\n^1[!!]^9 ^0Your VFMS license key is has been ^2validated^9\n");
+            }
+        }
     }
 }
