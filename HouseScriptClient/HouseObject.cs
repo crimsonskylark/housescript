c2493ff5b488bbe8967bf06cf3186ed13de9a7fa
crimsonskylark <47747084+crimsonskylark@users.noreply.github.com>
2020-07-15 09:42:54 -0300

    Changes to `HouseObject`.
    Mostly:
    
    - Getting the sell price % from a convar;
    - Added a houseId attribute
    - Added a propType attribute

--- HouseScriptClient/HouseObject.cs
+++ HouseScriptClient/HouseObject.cs
@@ -1,72 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
 
-namespace CarScriptClient
+namespace HouseScript.Client
 {
-    // <summary>
-    // This class represents an object that has been put in the house by the owner.
-    // Its purpose is to store the state in which the objects were created in a format
-    // suited for (de)serialization in order to allow for easy manipulation.
-    // </summary>
-    // <param name="prop_hash">The hash of the prop. Necessary in order to re-create the object on demand.</param>
-    // <param name="prop_pos">The position of the prop in the world represented by a <c>Vector3</b>.</param>
-    // <param name="prop_rotation">The rotation of the prop represented by a <c>Vector3</b>.</param>
-    // <param name="time_created">UNIX timestamp of when the object was first materialized.</param>
-    // <param name="owner">Rockstar license identifier of the player that owns this object.</param>
-    // <param name="price">The price that was paid for this object in the shop.</param>
-    // <param name="sell_price">The price that this object is going to be sold for. Defaults to 75% of the original <c>price</c>.
+    /* <summary>
+    /* This class represents an object that has been put in the house by the propOwner.
+    /* Its purpose is to store the state in which the objects were created in a format
+    /* suited for (de)serialization in order to allow for easy manipulation.
+    /* </summary>
+    /* <param name="propHash">The hash of the prop. Necessary in order to re-create the object on demand.</param>
+    /* <param name="propPos">The position of the prop in the world represented by a <c>Vector3</b>.</param>
+    /* <param name="propRotation">The rotation of the prop represented by a <c>Vector3</b>.</param>
+    /* <param name="propTimeCreated">UNIX timestamp of when the object was first materialized.</param>
+    /* <param name="propOwner">Rockstar license identifier of the player that owns this object.</param>
+    /* <param name="propPrice">The propPrice that was paid for this object in the shop.</param>
+    /* <param name="propSellPrice">The propPrice that this object is going to be sold for. Defaults to 75% of the original <c>propPrice</c>.
+    /* <param name="houseId">A single user can have multiple houses, so we take that into consideration.</param>
+    /* <param name="propType">An easy, human-readable name to be displayed in the interface.</param>
+    */
+
     public class HouseObject
     {
-        public int      prop_hash { get; set; }
-        public Vector3  prop_pos { get; set; }
-        public Vector3  prop_rotation { get; set; }
-        public ulong    time_created { get; private set; }
-        public string   owner { get; private set; }
-        public int      price { get; private set; }
-        public int      sell_price { get; private set; }
+        public int propHash { get; set; }
+        public Vector3 propPos { get; set; }
+        public Vector3 propRotation { get; set; }
+        public ulong propTimeCreated { get; private set; }
+        public string propOwner { get; private set; }
+        public int propPrice { get; private set; }
+        public int propSellPrice { get; private set; }
+        public int houseId { get; private set; }
+        public string propType { get; private set; }
         public HouseObject(int prop_hash, Vector3 prop_pos, Vector3 prop_rotation, ulong time_created, string owner, int price)
         {
-            this.prop_hash = prop_hash;
-            this.prop_pos = prop_pos;
-            this.prop_rotation = prop_rotation;
-            this.time_created = (time_created == 0) ? GetUNIXTimeStamp() : time_created;
-            this.owner = owner;
-            this.price = price;
-            this.sell_price = (int)(price * .75f);
-            
+            propHash = prop_hash;
+            propPos = prop_pos;
+            propRotation = prop_rotation;
+            propTimeCreated = (time_created == 0) ? GetUNIXTimeStamp() : time_created;
+            propOwner = owner;
+            propPrice = price;
+            try
+            {
+                propSellPrice = (int)(price * float.Parse(GetConvar("sell_price_percent", "75")) / 100);
+            }
+            catch (FormatException)
+            {
+                Debug.WriteLine(GetConvar("sell_price_percent", "75").ToString());
+                Debug.WriteLine("You configured an invalid value in the `sell_price_percent` convar.\n" +
+                                "Make sure the value is between 1 and 100, inclusive.");
+            }
+
         }
         // <summary>
-        // Helper function to get the UNIX timestamp used in <c>time_created</c>.
+        // Helper function to get the UNIX timestamp used in <c>propTimeCreated</c>.
         // </summary>
         private static ulong GetUNIXTimeStamp()
         {
-            return (ulong)(TimeZoneInfo.ConvertTimeToUtc(DateTime.UtcNow) -
-                   new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds;
+            return (ulong)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
         }
 
         public override string ToString()
         {
             const string obj = @"
-            Owner: {0}\n
-            Hash: {1}\n
-            Position: Vector3({2}, {3}, {4})\n
-            Rotation: Vector3({5}, {6}, {7})\n
-            Time created: {8}\n
-            Price: {9}\n
-            Sell price: {10}\n
+            Owner: {0}
+            Hash: {1}
+            Position: Vector3({2}, {3}, {4})
+            Rotation: Vector3({5}, {6}, {7})
+            Time created: {8}
+            Price: {9}
+            Sell price: {10}
             ";
             return string.Format(obj,
-                                owner,
-                                prop_hash,
-                                prop_pos.X, prop_pos.Y, prop_pos.Z,
-                                prop_rotation.X, prop_rotation.Y, prop_rotation.Z,
-                                time_created,
-                                price,
-                                sell_price);
+                                propOwner,
+                                propHash,
+                                propPos.X, propPos.Y, propPos.Z,
+                                propRotation.X, propRotation.Y, propRotation.Z,
+                                propTimeCreated,
+                                propPrice,
+                                propSellPrice);
         }
 
     }
